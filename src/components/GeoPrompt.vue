6458f50083b545139300e609ec618b4bca7a165f
nikhil fpaipl <frontendjr1@fpaipl.com>
2023-12-05 15:10:04 +0530

    prompt

--- src/components/GeoPrompt.vue
+++ src/components/GeoPrompt.vue
@@ -1,37 +1,32 @@
 <template>
     <div class="modal" id="geolocationPromptModal" tabindex="-1" aria-labelledby="geolocationPromptModalLabel"
-      aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="geolocationPromptModalLabel">Geolocation Permission</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <p>We need your permission to access your location. Please allow geolocation access to use this feature.</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary" @click="allowGeolocation">Allow</button>
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Deny</button>
-          </div>
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="geolocationPromptModalLabel">Geolocation Permission</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>We need your permission to access your location. Please allow geolocation access to use this feature.
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" @click="allowGeolocation">Allow</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Deny</button>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     methods: {
-      allowGeolocation() {
-        // Handle the logic when the user allows geolocation
-        // You may call the function to get the user's location here
-        // For example: this.$emit('geolocationAllowed');
-        this.$emit('geolocationAllowed');
-        // Close the modal using native Bootstrap method
-        const modal = new bootstrap.Modal(document.getElementById('geolocationPromptModal'));
-        modal.hide();
-      }
+        allowGeolocation() {
+            this.$emit('geolocationAllowed');
+        }
     }
-  };
-  </script>
+};
+</script>
   
